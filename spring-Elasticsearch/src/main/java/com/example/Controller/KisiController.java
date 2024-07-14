9d7f62ee66d61b279b8a22bcd9775e8856372ee8
HuseyinErkek <115713946+HuseyinErkek@users.noreply.github.com>
2024-07-14 22:51:28 +0300

    Elasticsearch created.

--- spring-Elasticsearch/src/main/java/com/example/Controller/KisiController.java
+++ spring-Elasticsearch/src/main/java/com/example/Controller/KisiController.java
@@ -1,2 +1,39 @@
-package com.example.Controller;public class KisiController {
+package com.example.Controller;
+
+
+import com.example.entity.Kisi;
+import com.example.repository.KisiRepository;
+import java.util.Calendar;
+import java.util.List;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/kisi")
+public class KisiController {
+
+    private final KisiRepository kisiRepository;
+
+    @PostConstruct // kisi olusturmak icin.
+    public void init(){
+        Kisi kisi =new Kisi();
+        kisi.setAd("Huseyin");
+        kisi.setSoyad("Erkek");
+        kisi.setAdres("Dirim Sok");
+        kisi.setDogumTarihi(Calendar.getInstance().getTime());
+        kisi.setId("K0001");
+        kisiRepository.save(kisi);
+    }
+
+    @GetMapping("/{search}")
+    public ResponseEntity<List<Kisi>> getKisi(@PathVariable String search) {
+        List<Kisi> kisiler = kisiRepository.findByAdLikeOrSoyadLike(search, search);
+        return ResponseEntity.ok(kisiler);
+    }
 }
